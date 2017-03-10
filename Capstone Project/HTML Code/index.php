ed071fe4fda813a0499ab14aaff77da418a1bb1f
karanchoud <s3378778@student.rmit.edu.au>
2017-03-10 17:23:38 +1100

    Add files via upload

---
+++ Capstone Project/HTML Code/index.php
@@ -0,0 +1,38 @@
+<?xml version="1.0" encoding="UTF-8" ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"> 
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+
+<head>
+</head>
+<title> Welcome to The Stock Market Trading Web Page </title>
+<link rel="stylesheet" type="text/css" href="styling.css" />
+<body>
+
+<div class="body">
+
+<div class="content">
+<!--<img src="LoginButton.png" alt="Login" style="width:100px;height:50px;"/>-->
+<img style="position:absolute; top:15px; left:1050px; width:100px; height:50px" src="LoginButton.png"/>
+<img style="position:absolute; top:15px; left:1250px; width:100px; height:50px" src="register-green.jpg"/>
+<h1> Stock Market </h1>
+</div>
+
+<!--<div class="content">
+<h2><i> Build No: 1.0 </i></h2>
+</div>
+-->
+
+<div class="menu">
+
+<ul>
+  <li><a href="#home">Home</a></li>
+  <li><a href="#searchstock">Search Stock</a></li>
+  <li><a href="#trade">Trade</a></li>
+  <li><a href="#myAccount">myAccount</a></li>
+</ul>
+
+</div>
+
+</div>
+</body>
+</html>
